390a19a65c1ce40bad35b139cd0b687d78ebed3c
Suleiman Moraes <suleimanmoraes@gmail.com>
2023-04-28 09:02:29 -0300

    Restirando tests de integração Resolvi deixar para um outro momento, e focar em outras coisas primeiro

--- src/test/java/br/com/simplecrud/SimpleCrudApplicationTests.java
+++ src/test/java/br/com/simplecrud/SimpleCrudApplicationTests.java
@@ -3,10 +3,10 @@ package br.com.simplecrud;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class SimpleCrudApplicationTests {
 
-	@Test
+	// @Test
 	void contextLoads() {
 	}
 
--- src/test/java/br/com/simplecrud/config/TestConfig.java
+++ src/test/java/br/com/simplecrud/config/TestConfig.java
@@ -5,6 +5,7 @@ import org.springframework.http.MediaType;
 public class TestConfig {
     
     public static final int SERVER_PORT = 9080;
+    public static final String LOCALHOST_8080 = "http://localhost:8080";
     public static final String HEADER_PARAM_ORIGIN = "Origin";
     public static final String HEADER_PARAM_AUTHORIZATION = "Authorization";
     public static final String CONTENT_TYPE_JSON = MediaType.APPLICATION_JSON_VALUE;
